45e4679d17502112ebe480593b864c6a5af67ce1
yunus erbek <yunus_erbek@windowslive.com>
2022-09-22 15:40:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bloomcompose/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.bloomcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val pink100 = Color(0xFFFFF1F1)
+val White = Color(0xFFFFFFFF)
+val Gray = Color(0xFF232323)
+val Pink900 = Color(0xFF3F2C2C)
+val White850 = Color(0x09FFFFFFF)
